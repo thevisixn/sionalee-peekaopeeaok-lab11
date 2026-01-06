6f776dff5b2598c0a4c63f3cfa42237bcbbc890d
visionalee <flook.xtnt@gmail.com>
2026-01-06 14:35:35 +0700

    เสร็จแล้ว

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+string grades[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W" };
+string random_grade(){
+    int grade = rand()%9;
+    return grades[grade];
+}
+
+
+
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << random_grade() << " in this 261102.";
+}
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
